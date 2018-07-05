2b962c9e97cd48d9e692b1acda90b2c1c723d8fb
MarcoLicona <hh0@live.com.mx>
2018-07-05 08:53:01 -0500

    Hola Marco usas Cgato pa programar

--- WindowsFormsApplication4/GUI/Form1.cs
+++ WindowsFormsApplication4/GUI/Form1.cs
@@ -1,13 +1,20 @@
-﻿using System;
+﻿using iTextSharp.text.pdf;
+using iTextSharp.text;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Base.DAO;
+using System.Xml.Linq;
 
 namespace WindowsFormsApplication4
 {
@@ -21,12 +28,95 @@ namespace WindowsFormsApplication4
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             dataGridView1.DataSource = obje.VistaTabla();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Document doc = new Document(PageSize.A4.Rotate(), 10, 10, 10, 10);
+            SaveFileDialog saverfileDialog1 = new SaveFileDialog();
+            saverfileDialog1.InitialDirectory = @"c:";
+            saverfileDialog1.Title = "guardar reporte";
+            saverfileDialog1.DefaultExt = "pdf";
+            saverfileDialog1.Filter = "pdf files(*.pdf)|*.pdf| all files(*.*)|*.*";
+            saverfileDialog1.FilterIndex = 2;
+            saverfileDialog1.RestoreDirectory = true;
+            string filename = "";
+            if (saverfileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                filename = saverfileDialog1.FileName;
+            }
+            if (filename.Trim() != "")
+            {
+                FileStream file = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                PdfWriter.GetInstance(doc, file);
+                doc.Open();
+                string envio = "Fecha:" + DateTime.Now.ToString();
+
+                Chunk chunk = new Chunk("Proveedores", FontFactory.GetFont("ARIAL", 20, iTextSharp.text.Font.BOLD));
+                doc.Add(new Paragraph(chunk));
+                doc.Add(new Paragraph("                       "));
+                doc.Add(new Paragraph("                       "));
+                doc.Add(new Paragraph("--------------------------"));
+                doc.Add(new Paragraph("Vision "));
+                doc.Add(new Paragraph("Marco y kim"));
+                doc.Add(new Paragraph(envio));
+                doc.Add(new Paragraph("--------------------------"));
+                doc.Add(new Paragraph("                       "));
+                doc.Add(new Paragraph("                       "));
+                doc.Add(new Paragraph("                       "));
+                GenerarDocumento(doc);
+                doc.AddCreationDate();
+                doc.Add(new Paragraph("Haber si jala", FontFactory.GetFont("ARIAL", 20, iTextSharp.text.Font.BOLD)));
+                doc.Close();
+                Process.Start(filename);//Esta parte se puede omitir, si solo se desea guardar el archivo, y que este no se ejecute al instante
+            }
+
+        }
+        public void GenerarDocumento(Document document)
+        {
+            int i, j;
+            PdfPTable datatable = new PdfPTable(dataGridView1.ColumnCount);
+            datatable.DefaultCell.Padding = 3;
+            float[] headerwidths = GetTamañoColumnas(dataGridView1);
+            datatable.SetWidths(headerwidths);
+            datatable.WidthPercentage = 100;
+            datatable.DefaultCell.BorderWidth = 2;
+            datatable.DefaultCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            for (i = 0; i < dataGridView1.ColumnCount; i++)
+            {
+                datatable.AddCell(dataGridView1.Columns[i].HeaderText);
+            }
+            datatable.HeaderRows = 1;
+            datatable.DefaultCell.BorderWidth = 1;
+            for (i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                for (j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    if (dataGridView1[j, i].Value != null)
+                    {
+                        datatable.AddCell(new Phrase(dataGridView1[j, i].Value.ToString()));//En esta parte, se esta agregando un renglon por cada registro en el datagrid
+                    }
+                }
+                datatable.CompleteRow();
+            }
+            document.Add(datatable);
+        }
+        public float[] GetTamañoColumnas(DataGridView dg)
+        {
+            float[] values = new float[dg.ColumnCount];
+            for (int i = 0; i < dg.ColumnCount; i++)
+            {
+                values[i] = (float)dg.Columns[i].Width;
+            }
+            return values;
+        }
+
     }
 }
